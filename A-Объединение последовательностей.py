7dda82918f520cd4a96c9b1c1a41b558aee4cf30
Makar Baderko <makarbaderko@gmail.com>
2020-06-01 09:21:48 +0300

    Made a solution to task 1.1

--- A-Объединение последовательностей.py
+++ A-Объединение последовательностей.py
@@ -1,8 +1,11 @@
 x = int(input())
 a = []
 b = []
-for i in range(x):
-    a.append(i^2)
-    b.append(i^3)
-c = list(set(a + b)).sort()
-print(c[x])
+for i in range(1, x+1):
+    a.append(i**2)
+    b.append(i**3)
+c = list(set(a + b))
+if x!= 1:
+    print(c[x])
+if x==1:
+    print(1)
